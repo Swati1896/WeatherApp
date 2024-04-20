72a0a1857bbf6fdadcbd6ed95aa8a7b1d6b33465
Pankaj Bhakre <pankajbhakre11@gmail.com>
2024-04-20 14:50:27 +0530

    Weather App Changes

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d3447a3ede1376a546861e8bc0991c2e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
